6286ebf15a714a0cc0b2e53c1fa3c3d773e03dae
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-11 23:48:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["wow", "I", "am", "really", "learning"]
-  << "arrays!"
+  add_element_to_end_of_array ["wow", "I", "am", "really", "learning"]
+ puts add_element_to_end_of_array << "arrays!"
  end
 
 def add_element_to_start_of_array(array, element)
